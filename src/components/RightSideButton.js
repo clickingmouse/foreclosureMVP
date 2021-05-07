0f36b5e0dc0819105b5ebb01a17b42dfa5496d42
Kevin Ko <kevin.alex.ko@gmail.com>
2021-05-07 15:01:24 +0800

    3

---
+++ src/components/RightSideButton.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image,Button,TouchableOpacity } from 'react-native';
+import * as Font from 'expo-font';
+//import { AppLoading } from 'expo';
+import { useFonts } from 'expo-font';
+
+const RightSideButton = (props)=>{
+    let [fontsLoaded] = useFonts({
+        'helvetica':require( '../assets/fonts/Helvetica.ttc'),
+      });
+const propStyles={
+    backgroundColor:props.backgroundColor
+}
+    const { backgroundColor, color} = props;
+    const {capsuleButtonText, capsuleButtonContainer} = styles;
+//    const combineStyles = StyleSheet.combine([capsuleButtonContainer, backgroundColor])
+
+
+    return(
+        <TouchableOpacity onPress={props.onPress} style={[styles.RSButtonContainer, propStyles]}
+        >
+        <Text style={styles.RSButtonText}>{props.title}</Text>
+      </TouchableOpacity>
+    )
+}
+
+export default RightSideButton
+
+const styles = StyleSheet.create({
+    
+    RSButtonContainer: {
+    flexDirection:'row',
+        elevation: 8,
+    //backgroundColor: "#009688",
+    borderRadius: 30,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    width: 94.00,
+    height: 27.20,
+    //opacity: 0,
+    borderColor: "#A88663",
+    borderWidth:1
+  },
+  RSButtonText: {
+    fontSize: 12,
+    color: "#A88663",
+    fontWeight: 300,
+    alignSelf: "center",
+    //textTransform: "uppercase",
+  },
+})
--- src/screens/ExploreDetails.js
+++ src/screens/ExploreDetails.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-
-
-
+import { StyleSheet, Text, View, Image,Button,TouchableOpacity } from 'react-native';
+import * as Font from 'expo-font';
+//import { AppLoading } from 'expo';
+import { useFonts } from 'expo-font';
+import CapsuleButton from '../components/CapsuleButton'
+import RightSideButton from '../components/RightSideButton'
 const GalleryPlaceholder = ()=>{
   return(<View >
 <Image source = {require('../assets/img/placeholders/property.png')}
@@ -11,19 +13,147 @@ style={{resizeMode:'cover', height:287, width:'100%'}}/>
   </View>)
 }
 
+const MenuBar = ()=>{
+  return(
+    <View style={{flexDirection:'row'}}>
+      <Button/><Button/><Button/><Button/>
+    </View>
+  )
+}
+
+const PropertyDetailLeft =()=>{
+  return (
+    <View>    
+      <View style={{flexDirection:'row'}}>
+      <Text>8,600,000</Text>
+    <Text>HKD</Text>
+    <Text>Valuation</Text>
+    </View>
+    <Text>Tseung Kwan O</Text>
+    <View style={{flexDirection:'row'}}>
+      <Text>2 </Text>
+    <Text>2 </Text>
+    <Text>680sqft </Text>
+    </View>
+
+    </View>
+
+    
+    )
+}
+const PropertyDetailRight =()=>{
+  return (
+  <View>
+  <Text>Residential</Text>
+  <RightSideButton title='Floorplan' />
+  {/*<Button title='Floor-plan'/>*/}
+  </View>
+  )
+}
+
+const PropertyDetailContentTop=()=>{
+  return(
+    <View style={{flexDirection:'row', justifyContent:'space-between'}}>
+<PropertyDetailLeft/>
+<PropertyDetailRight/>
+    </View>
+  )
+}
+
+const PropertyDetailModal=()=>{
+  return(<Text>PropertyDetails</Text>)
+}
+const PropertyDetailCurrentBid=()=>{  
+  return(<Text>CurrentBid</Text>)
+}
+
+const EnquireButton =({ onPress, title })=>{
+  return(
+  <TouchableOpacity onPress={onPress} style={styles.enquireButtonContainer}>
+  <Text style={styles.enquireButtonText}>{title}</Text>
+</TouchableOpacity>
+  )
+}
+
+const PropertyDetailContent =()=>{
+  return(
+    <View>
+      <PropertyDetailContentTop/>
+      <PropertyDetailModal/>
+      <PropertyDetailCurrentBid/>
+      <View style={{flexDirection:'row'}}>
+        <EnquireButton onPress = {()=>console.log('enquire')} title='Enquire'/>
+        <CapsuleButton title='Place your bid' backgroundColor='#A88663'/>
+        {/*<Button title='Enquire' style={styles.enquire}/>
+        <Button title='Place your bid' />*/}
+      </View>
+    </View>
+  )
+
+}
+
 
 
 export default function ExploreDetails(data={data}) {
+
+  let [fontsLoaded] = useFonts({
+    'helvetica':require( '../assets/fonts/Helvetica.ttc'),
+  });
+
+  if (fontsLoaded){ 
     return (
       <View style={{flex:1}}>
         <View style={{height:287}}>
         <GalleryPlaceholder/>
         </View>
+        <MenuBar/>
         <Text>PropertyDetails</Text>
+        <PropertyDetailContent/>
+        
         <StatusBar style="auto" />
       </View>
     );
-  }
+  }else { return(<Text>loading</Text>)}
+ } 
+
+  const styles = StyleSheet.create({
+      appButtonContainer: {
+      elevation: 8,
+      backgroundColor: "#009688",
+      borderRadius: 30,
+      paddingVertical: 10,
+      paddingHorizontal: 12,
+      width: 160.00,
+      height: 37.11,
+    },
+    appButtonText: {
+      fontSize: 16,
+      fontFamily:'helvetica',
+      color: "#fff",
+      fontWeight: "bold",
+      alignSelf: "center",
+      textTransform: "uppercase"
+    },
+    enquireButtonContainer: {
+
+      elevation: 8,
+      backgroundColor: "#000",
+      borderRadius: 30,
+      paddingVertical: 10,
+      paddingHorizontal: 12,
+      width: 160.00,
+      height: 37.11,
+  
+    },
+    enquireButtonText: {
+      fontSize: 16,
+      color: "#fff",
+      fontWeight: 300,
+      alignSelf: "center",
+      //textTransform: "uppercase"
+    },
+  });
+
 
   const data={
     propertyID:'001',
